16bcf45f3aead207783f50bb907e725e49f6819a
wilson <wilsonquispe792@gmail.com>
2019-06-05 14:08:57 -0300

    Imagen de fondo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Barra from './componentes/Barra';
+import ImagenFondoInicio from './componentes/ImagenFondoInicio';
+
+
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <Barra></Barra>
+        <ImagenFondoInicio></ImagenFondoInicio>
+        
+      </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/ImagenFondoInicio.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+import imagen from '../imagenes/ap-web.jpg';
+
+const ImagenFondoInicio = () => <Image src={imagen} fluid />
+
+export default ImagenFondoInicio;
